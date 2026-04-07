76f6f7de6594f4784bfa6a94086ab15894f0af28
DawnRighteous21 <ganduytran2102@gmail.com>
2026-04-07 11:30:36 +0700

    Organized projects and completed Exercise 1 & 2

---
+++ SchoolAppCoreRazor/Models/Enrollment.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SchoolAppCoreRazor.Models
+{
+    public class Enrollment
+    {
+        public int EnrollmentID { get; set; }
+        public int CourseID { get; set; }
+        public int StudentID { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Grade { get; set; }
+        public Course? Course { get; set; }
+        public Student? Student { get; set; }
+    }
+}
---
+++ SchoolAppCoreRazor/Program.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using SchoolAppCoreRazor.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+
+builder.Services.AddDbContext<SchoolContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SchoolDBConnectionString")));
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
